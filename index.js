b78ca65b3a8590fbf1f27d2104dd1e1e89595bc3
AncyFit <154664615+AncyFit@users.noreply.github.com>
2023-12-23 16:13:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+//var router=require('./router/Beds')
+const bodyParser = require('body-parser');
+mongoose.set('strictQuery',false)
+const url = "mongodb://127.0.0.1:27017/Beds";
 
-const url = "mongodb://127.0.0.1:27017/BestHospital";
-mongoose.connect(url );
+mongoose.connect(url,{useNewUrlParser:true})
+const con = mongoose.connection
 
-const con = mongoose.connection;
-con.on('open', () => {
-  console.log("db is connected");
-});
+con.on('open',()=>{
+    console.log('connected.......');
+})
 
+app.use(express.json())
+app.use(bodyParser.json())
 
-
-
-
-
-
-app.use(express.json());
-
-// Import and use your router files
 const alienrouter = require('./router/Beds');
-app.use('/', alienrouter);
+const atrouter=require('./router/Dash')
+app.use('/',alienrouter);
+app.use('/',atrouter);
+//app.use(router)
 
-app.listen(7000, () => {
-  console.log('Server is running on port 7000'); // Confirm that the server is running
-});
+app.listen(9000,()=>{
+    console.log('Server is running');
+})
