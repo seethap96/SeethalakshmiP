d47ee71d748472dc9f46002f6c7964fbc40bc73b
seethap96 <142819900+seethap96@users.noreply.github.com>
2024-01-29 11:05:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-//var router=require('./router/Beds')
-const bodyParser = require('body-parser');
-mongoose.set('strictQuery',false)
-const url = "mongodb://127.0.0.1:27017/Beds";
 
-mongoose.connect(url,{useNewUrlParser:true})
-const con = mongoose.connection
+const url = "mongodb://127.0.0.1:27017/Bedss";
+mongoose.connect(url );
 
-con.on('open',()=>{
-    console.log('connected.......');
-})
+const con = mongoose.connection;
+con.on('open', () => {
+  console.log("db is connected");
+});
 
-app.use(express.json())
-app.use(bodyParser.json())
 
+
+app.use(express.json());
+
+// Import and use your router files
 const alienrouter = require('./router/Beds');
-const atrouter=require('./router/Dash')
-app.use('/',alienrouter);
-app.use('/',atrouter);
-//app.use(router)
+app.use('/', alienrouter);
 
-app.listen(9000,()=>{
-    console.log('Server is running');
-})
+app.listen(9000, () => {
+  console.log('Server is running on port 9000'); // Confirm that the server is running
+});
